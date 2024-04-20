790cc06a09a888b85f0822f04f378842ae8e66f7
robopng <109000271+robopng@users.noreply.github.com>
2024-04-19 20:17:51 -0500

    Begin adding functionality for a graphing component, and clean up the method for creating panels

---
+++ src/simplejavacalculator/GraphCalculator.java
@@ -0,0 +1,18 @@
+package simplejavacalculator;
+
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
+public class GraphCalculator {
+    public GraphCalculator(){
+        double[] xData = new double[] { 0.0, 1.0, 2.0 };
+        double[] yData = new double[] { 2.0, 1.0, 0.0 };
+
+        // Create Chart
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+
+        // Show it
+        new SwingWrapper(chart).displayChart();
+    }
+}
--- src/simplejavacalculator/UI.java
+++ src/simplejavacalculator/UI.java
@@ -35,25 +35,22 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.*;
 
+
 public class UI implements ActionListener, KeyListener {
    
    private final JFrame frame;
+   private final JFrame gFrame;
    
    private final JPanel panel;
-   private final JPanel panelSub1;
-   private final JPanel panelSub2;
-   private final JPanel panelSub3;
-   private final JPanel panelSub4;
-   private final JPanel panelSub5;
-   private final JPanel panelSub6;
-   private final JPanel panelSub7;
-   private final JPanel panelSub8;
-   
-   private final JTextArea text;
+   private final JPanel gPanel;
+
+    private final JTextArea text;
+    private final JTextArea gText;
 
    // convert this into but holding all the buttons?
    // might be nicer for cleanliness of code later on
-   private final JButton[] but;
+   private final Calculator calc;
+    private final JButton[] but;
     private final JButton butAdd;
     private final JButton butMinus;
     private final JButton butMultiply;
@@ -73,8 +70,16 @@ public class UI implements ActionListener, KeyListener {
     private final JButton butBinary;
     private final JButton butln;
     private final JButton butKeyboard;
-   private final Calculator calc;
-   
+   private final GraphCalculator gCalc;
+    private final JButton[] gBut;
+    private final JButton butGraph;
+    private final JButton butGraphClose;
+    private final JButton butGraphComma;
+    private final JButton butGraphGo;
+    private final JButton butGraphScatter;
+    private final JButton butGraphLine;
+    private final JButton butGraphAdd;
+
    private final String[] buttonValue = {"0", "1", "2", "3", "4", "5", "6",
       "7", "8", "9"};
    
@@ -85,30 +90,27 @@ public class UI implements ActionListener, KeyListener {
 
     public UI() throws IOException {
       frame = new JFrame("Calculator PH");
+      gFrame = new JFrame("Graphing Component");
 
       BufferedImageCustom imageReturn = new BufferedImageCustom();
       image = new ImageIcon(imageReturn.imageReturn());      
       
       panel = new JPanel();
+      gPanel = new JPanel();
       panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-      panelSub1 = new JPanel(new FlowLayout());
-      panelSub2 = new JPanel(new FlowLayout());
-      panelSub3 = new JPanel(new FlowLayout());
-      panelSub4 = new JPanel(new FlowLayout());
-      panelSub5 = new JPanel(new FlowLayout());
-      panelSub6 = new JPanel(new FlowLayout());
-      panelSub7 = new JPanel(new FlowLayout());
-      panelSub8 = new JPanel(new FlowLayout());
-      
+
       font = new Font("Consolas",Font.PLAIN, 18);
       
       text = new JTextArea(1, 30);
+      gText = new JTextArea(1, 30);
       
       textFont = new Font("Consolas",Font.BOLD, 24);
       
-      but = new JButton[10];      
+      but = new JButton[10];
+      gBut = new JButton[10];
       for (int i = 0; i < 10; i++) {
     		 but[i] = new JButton(String.valueOf(i));
+             gBut[i] = new JButton(String.valueOf(i));
       }      
       butAdd = new JButton("+");      
       butMinus = new JButton("-");      
@@ -129,124 +131,154 @@ public class UI implements ActionListener, KeyListener {
       butCancel = new JButton("C");      
       butBinary = new JButton("Bin");
       butKeyboard = new JButton("Key");
-      // add a decimal!!
-      
+
+      butGraph = new JButton("Graph");
+      butGraphScatter = new JButton("Scatter");
+      butGraphLine = new JButton("Line");
+      butGraphComma = new JButton(",");
+      butGraphAdd = new JButton("Add");
+      butGraphGo = new JButton("Go");
+      butGraphClose = new JButton("Close");
+
       calc = new Calculator();
-      
+      gCalc = new GraphCalculator();
    }
    
    public void init() {      
-      frame.setSize(450, 450);
-      frame.setLocationRelativeTo(null); 
-      frame.setResizable(false);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setIconImage(image.getImage());
+        frame.setSize(450, 450);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setIconImage(image.getImage());
+        // for consistency across the two panels
+        gFrame.setSize(450, 450);
+        gFrame.setLocationRelativeTo(null);
+        gFrame.setResizable(false);
+        gFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gFrame.setIconImage(image.getImage());
 
-      text.setFont(textFont);
-      text.setEditable(false);
+        text.setFont(textFont);
+        text.setEditable(false);
 
-      for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
          but[i].setFont(font);
-      }
-      // this is where having an array might reduce the size of the code a lot...
-      butAdd.setFont(font);
-      butMinus.setFont(font);
-      butMultiply.setFont(font);
-      butDivide.setFont(font);
-      butEqual.setFont(font);
-      butSquareRoot.setFont(font);
-      butSquare.setFont(font);
-      butOneDividedBy.setFont(font);
-      butCos.setFont(font);
-      butSin.setFont(font);
-      butTan.setFont(font);
-      butln.setFont(font);
-      butxpowerofy.setFont(font);
-      butlog.setFont(font);
-      butrate.setFont(font);
-      butabs.setFont(font);
-      butCancel.setFont(font);
-      butBinary.setFont(font);
-      butKeyboard.setFont(font);
+        }
+        // this is where having an array might reduce the size of the code a lot...
+        butAdd.setFont(font);
+        butMinus.setFont(font);
+        butMultiply.setFont(font);
+        butDivide.setFont(font);
+        butEqual.setFont(font);
+        butSquareRoot.setFont(font);
+        butSquare.setFont(font);
+        butOneDividedBy.setFont(font);
+        butCos.setFont(font);
+        butSin.setFont(font);
+        butTan.setFont(font);
+        butln.setFont(font);
+        butxpowerofy.setFont(font);
+        butlog.setFont(font);
+        butrate.setFont(font);
+        butabs.setFont(font);
+        butCancel.setFont(font);
+        butBinary.setFont(font);
+        butKeyboard.setFont(font);
+        butGraph.setFont(font);
 
-      panel.add(Box.createHorizontalStrut(100));
-      panelSub1.add(text);
-      panel.add(panelSub1);
-      
-      panelSub2.add(but[1]);
-      panelSub2.add(but[2]);
-      panelSub2.add(but[3]);
-      panelSub2.add(Box.createHorizontalStrut(15));
-      panelSub2.add(butAdd);
-      panelSub2.add(butMinus);
-      panel.add(panelSub2);
-      
-      panelSub3.add(but[4]);
-      panelSub3.add(but[5]);
-      panelSub3.add(but[6]);
-      panelSub3.add(Box.createHorizontalStrut(15));
-      panelSub3.add(butMultiply);
-      panelSub3.add(butDivide);      
-      panel.add(panelSub3);
-      
-      panelSub4.add(but[7]);
-      panelSub4.add(but[8]);
-      panelSub4.add(but[9]);
-      panelSub4.add(Box.createHorizontalStrut(15));
-      panelSub4.add(butEqual);
-      panelSub4.add(butCancel);
-      panel.add(panelSub4);
-      
-      panelSub5.add(Box.createHorizontalStrut(92));
-      panelSub5.add(but[0]);
-      panelSub5.add(butln); 
-      panelSub5.add(Box.createHorizontalStrut(210));
-      panel.add(panelSub5);
-      
-      panelSub6.add(butSquare);
-      panelSub6.add(butSquareRoot);
-      panelSub6.add(butOneDividedBy);
-      panelSub6.add(butxpowerofy);
-      panel.add(panelSub6);
-      
-      panelSub7.add(butCos);
-      panelSub7.add(butSin);
-      panelSub7.add(butTan);
-      panel.add(panelSub7);
-      
-      panelSub8.add(butlog);
-      panelSub8.add(butrate);
-      panelSub8.add(butabs);
-      panelSub8.add(butBinary);
-      panelSub8.add(butKeyboard);
-      panel.add(panelSub8);
-      
-      for (int i = 0; i < 10; i++) {
-         but[i].addActionListener(this);
-      }      
-      butAdd.addActionListener(this);
-      butMinus.addActionListener(this);
-      butMultiply.addActionListener(this);
-      butDivide.addActionListener(this);
-      butSquare.addActionListener(this);
-      butSquareRoot.addActionListener(this);
-      butOneDividedBy.addActionListener(this);
-      butCos.addActionListener(this);
-      butSin.addActionListener(this);
-      butTan.addActionListener(this);
-      butln.addActionListener(this); 
-      butxpowerofy.addActionListener(this);
-      butlog.addActionListener(this);
-      butrate.addActionListener(this);
-      butabs.addActionListener(this);
-      butBinary.addActionListener(this);
-      butKeyboard.addKeyListener(this);
-      butEqual.addActionListener(this);
-      butCancel.addActionListener(this);
-      
-      frame.add(panel);
-      frame.setVisible(true);
+        // panel creation for main calculator panel
+        panel.add(Box.createHorizontalStrut(100));
+        JPanel textSub = new JPanel(new FlowLayout());
+        textSub.add(text);
+        panel.add(textSub);
+        // visualization of panel layouts through arrays
+        // mainPanelSubs => any panel that goes like X X X   X X
+        JButton[][][] mainPanelSubs = new JButton[][][]{
+                {{but[1], but[2], but[3]}, {butAdd, butMinus}},
+                {{but[4], but[5], but[6]}, {butMultiply, butDivide}},
+                {{but[7], but[8], but[9]}, {butEqual, butCancel}},
+        };
+        // sidePanelSubs => any panel that goes like X X X X X
+        JButton[][] sidePanelSubs = new JButton[][]{
+                {but[0], butln, butGraph},
+                {butSquare, butSquareRoot, butOneDividedBy, butxpowerofy},
+                {butCos, butSin, butTan},
+                {butlog, butrate, butabs, butBinary, butKeyboard}
+        };
+        // if greater control over button placement is desired remove these
+        // for now they are a fun and easy simplification of the messy code
+        for (JButton[][] panelSub : mainPanelSubs){
+            panel.add(this.getPanelSub(panelSub[0], panelSub[1]));
+        }
+        for (JButton[] panelSub : sidePanelSubs){
+            panel.add(this.getPanelSub(panelSub));
+        }
+
+        // panel creation for graphing component panel
+        JButton[][][] gMainPanelSubs = new JButton[][][]{
+                {{gBut[1], gBut[2], gBut[3]}, {butGraphScatter, butGraphLine}},
+                {{gBut[4], gBut[5], gBut[6]}, {butGraphComma, butGraphAdd }},
+                {{gBut[7], gBut[8], gBut[9]}, {butGraphGo, butGraphClose}},
+        };
+        gPanel.add(Box.createHorizontalStrut(100));
+        JPanel gTextSub = new JPanel(new FlowLayout());
+        gTextSub.add(gText);
+        panel.add(gTextSub);
+        for (JButton[][] panelSub : gMainPanelSubs){
+            gPanel.add(this.getPanelSub(panelSub[0], panelSub[1]));
+        }
+
+        // can we get something like this for all the buttons?
+        for (JButton button : but){
+            button.addActionListener(this);
+        }
+        butAdd.addActionListener(this);
+        butMinus.addActionListener(this);
+        butMultiply.addActionListener(this);
+        butDivide.addActionListener(this);
+        butSquare.addActionListener(this);
+        butSquareRoot.addActionListener(this);
+        butOneDividedBy.addActionListener(this);
+        butCos.addActionListener(this);
+        butSin.addActionListener(this);
+        butTan.addActionListener(this);
+        butln.addActionListener(this);
+        butxpowerofy.addActionListener(this);
+        butlog.addActionListener(this);
+        butrate.addActionListener(this);
+        butabs.addActionListener(this);
+        butBinary.addActionListener(this);
+        butKeyboard.addKeyListener(this);
+        butGraph.addActionListener(this);
+
+        butEqual.addActionListener(this);
+        butCancel.addActionListener(this);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        gFrame.add(gPanel);
    }
+
+   private JPanel getPanelSub(JButton[] buttons){
+        JPanel panelSub = new JPanel(new FlowLayout());
+        for (JButton button : buttons){
+            panelSub.add(button);
+        }
+        return panelSub;
+   }
+
+   private JPanel getPanelSub(JButton[] firstSegButtons, JButton[] secondSegButtons){
+        int panelSeparatorWidth = 15;
+        JPanel panelSub = new JPanel(new FlowLayout());
+        for (JButton button : firstSegButtons){
+            panelSub.add(button);
+        }
+        panelSub.add(Box.createHorizontalStrut(panelSeparatorWidth));
+        for (JButton button : secondSegButtons){
+            panelSub.add(button);
+        }
+        return panelSub;
+    }
 
    @Override
    public void actionPerformed(ActionEvent e) {
@@ -331,6 +363,10 @@ public class UI implements ActionListener, KeyListener {
 
          if (source == butBinary)
             parseToBinary();
+
+         if (source == butGraph)
+            gFrame.setVisible(true);
+
       }
 
       text.selectAll();
@@ -351,8 +387,8 @@ public class UI implements ActionListener, KeyListener {
         char keyChar = e.getKeyChar();
         try {
             text.replaceSelection(buttonValue[Character.getNumericValue(keyChar)]);
-        } catch (IllegalArgumentException ignored) {
-
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+            // non-numeric keys will exceed the 0-10 range of buttonValue
         }
     }
 
